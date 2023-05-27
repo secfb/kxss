e1a7ac0884d90332fff052fd94ba67f3b88a865b
DIMOOON <68978608+reewardius@users.noreply.github.com>
2023-05-27 23:52:30 +0300

    Create kxss.go

---
+++ kxss.go
@@ -0,0 +1,216 @@
+package main
+
+import (
+	"bufio"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"sync"
+	"time"
+)
+
+type paramCheck struct {
+	url   string
+	param string
+}
+
+var transport = &http.Transport{
+	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	DialContext: (&net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: time.Second,
+		DualStack: true,
+	}).DialContext,
+}
+
+var httpClient = &http.Client{
+	Transport: transport,
+}
+
+func main() {
+	httpClient.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	linksFile := flag.String("l", "", "Path to the file containing links")
+	flag.Parse()
+
+	sc := bufio.NewScanner(os.Stdin)
+	if *linksFile != "" {
+		file, err := os.Open(*linksFile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to open links file: %v\n", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+
+		sc = bufio.NewScanner(file)
+	}
+
+	initialChecks := make(chan paramCheck, 40)
+	appendChecks := makePool(initialChecks, func(c paramCheck, output chan paramCheck) {
+		reflected, err := checkReflected(c.url)
+		if err != nil {
+			return
+		}
+
+		if len(reflected) == 0 {
+			return
+		}
+
+		for _, param := range reflected {
+			output <- paramCheck{c.url, param}
+		}
+	})
+
+	charChecks := makePool(appendChecks, func(c paramCheck, output chan paramCheck) {
+		wasReflected, err := checkAppend(c.url, c.param, "iy3j4h234hjb23234")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error from checkAppend for URL %s with param %s: %s", c.url, c.param, err)
+			return
+		}
+
+		if wasReflected {
+			output <- paramCheck{c.url, c.param}
+		}
+	})
+
+	done := makePool(charChecks, func(c paramCheck, output chan paramCheck) {
+	output_of_url := []string{c.url, c.param}
+	filtered := true
+
+	for _, char := range []string{"\"", "'", "<", ">", "$", "|", "(", ")", "`", ":", ";", "{", "}", "%"} {
+		wasReflected, err := checkAppend(c.url, c.param, "aprefix"+char+"asuffix")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error from checkAppend for URL %s with param %s with %s: %s", c.url, c.param, char, err)
+			continue
+		}
+
+		if wasReflected {
+			output_of_url = append(output_of_url, char)
+			filtered = false
+		}
+	}
+
+	if !filtered && len(output_of_url) >= 2 {
+		fmt.Printf("URL: %s Param: %s Unfiltered: %v \n", output_of_url[0], output_of_url[1], output_of_url[2:])
+	}
+})
+
+
+	for sc.Scan() {
+		initialChecks <- paramCheck{url: sc.Text()}
+	}
+
+	close(initialChecks)
+	<-done
+}
+
+func checkReflected(targetURL string) ([]string, error) {
+	out := make([]string, 0)
+
+	req, err := http.NewRequest("GET", targetURL, nil)
+	if err != nil {
+		return out, err
+	}
+
+	req.Header.Add("User-Agent", "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36")
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return out, err
+	}
+	if resp.Body == nil {
+		return out, err
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return out, err
+	}
+
+	if strings.HasPrefix(resp.Status, "3") {
+		return out, nil
+	}
+
+	ct := resp.Header.Get("Content-Type")
+	if ct != "" && !strings.Contains(ct, "html") {
+		return out, nil
+	}
+
+	body := string(b)
+
+	u, err := url.Parse(targetURL)
+	if err != nil {
+		return out, err
+	}
+
+	for key, vv := range u.Query() {
+		for _, v := range vv {
+			if !strings.Contains(body, v) {
+				continue
+			}
+
+			out = append(out, key)
+		}
+	}
+
+	return out, nil
+}
+
+func checkAppend(targetURL, param, suffix string) (bool, error) {
+	u, err := url.Parse(targetURL)
+	if err != nil {
+		return false, err
+	}
+
+	qs := u.Query()
+	val := qs.Get(param)
+
+	qs.Set(param, val+suffix)
+	u.RawQuery = qs.Encode()
+
+	reflected, err := checkReflected(u.String())
+	if err != nil {
+		return false, err
+	}
+
+	for _, r := range reflected {
+		if r == param {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+type workerFunc func(paramCheck, chan paramCheck)
+
+func makePool(input chan paramCheck, fn workerFunc) chan paramCheck {
+	var wg sync.WaitGroup
+
+	output := make(chan paramCheck)
+	for i := 0; i < 40; i++ {
+		wg.Add(1)
+		go func() {
+			for c := range input {
+				fn(c, output)
+			}
+			wg.Done()
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(output)
+	}()
+
+	return output
+}
